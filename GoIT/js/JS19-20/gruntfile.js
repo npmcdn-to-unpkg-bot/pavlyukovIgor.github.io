3cc0c9476e52cb4a3f92803b42f34ab4145d0220
pavlyukovIgor <pavlyukov.igor@gmail.com>
2016-03-08 14:45:48 +0200

    JS 19-20

--- GoIT/js/JS19-20/gruntfile.js
+++ GoIT/js/JS19-20/gruntfile.js
@@ -47,15 +47,26 @@ watch: {
     sass: {
       files: ['src/style/*.scss'],
       tasks: ['sass'],
-    },
-  }
+    }
+  },
+imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'src/img/carousel',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'src/img/carousel'
+        }]
+    }
+}
 });
   // grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
 
-  grunt.registerTask('default', ['uglify','cssmin', 'sass']);
+  grunt.registerTask('default', ['uglify','cssmin', 'sass','imagemin']);
 };
