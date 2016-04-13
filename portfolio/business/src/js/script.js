be4579b9d3cd3ec4dda4a430c812e75921fdf5c1
pavlyukovIgor <pavlyukov.igor@gmail.com>
2016-04-13 19:29:35 +0300

    in working

---
+++ portfolio/business/src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".call__box").mouseout(function(e)
+  {
+   $(".call__link").css({"color": "#58c7f2", "border-bottom": "1px dashed #58c7f2"});
+  });
+  $(".call__box").mousemove(function(e)
+  {
+   $(".call__link").css({"color": "#fff", "border-bottom": "1px dashed #fff"});
+  });
+});
