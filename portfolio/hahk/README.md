7dadfd643986658e9553d6e293ddec0835ae0394
pavlyukovIgor <pavlyukov.igor@gmail.com>
2016-08-09 11:09:45 +0300

    in working

--- portfolio/hahk/README.md
+++
@@ -1,6 +0,0 @@
-<a href="http://pavlyukovigor.github.io/portfolio/freelance%20template/build/index.html">View site</a>  
-
-No adaptive for all screens<br>
-Technologies: HTML/HTML 5, CSS/CSS 3<br>
-Frameworks, libraries and plugins:  bootstrap3, SASS<br>
-Development Tools: Adobe Products, Atom, Command line, PixelPerfect<br>
--- portfolio/hahk/src/js/script.js
+++ portfolio/hahk/src/js/script.js
@@ -1,19 +1,11 @@
 $(function() {
   $('.single-item').slick({
     // autoplay: true,
-    arrows: fa,
+    arrows: false,
     dots: true,
-    infinite: true,
+    mobileFirst: true,
   });
     });
-
-
-        $(function() {
-               $('#link').click(function() {
-                   $('#menu').show();
-                   return false;
-               });
-           });
 
 
     $(function(){
@@ -21,9 +13,24 @@ $(function() {
     type: 'GET',
     url: 'http://dev.frevend.com/json/users.json',
     dataType: 'jsonp',
-    success: function(data) { console.log('Success!'); },
+    success: function() { console.log('Success!'); },
     error: function() { console.log('Uh Oh!'); },
     jsonp: 'jsonp'
 });
 
    });
+
+   function GoogleCallback(data) {
+
+
+       for (var i = 0; i < data.results.length; i++) {
+
+         $('.account__name', {
+           'text': data.results[i].name,
+           'appendTo': '.account',
+         });
+
+         $('<p>').appendTo('.account').addClass('.account__name').html(data.results[i].name);
+
+       };
+     };
